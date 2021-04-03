522bedce11a6648e79e9516245e676dfb3dd7e5a
JianMin-Xie <854377742@qq.com>
2021-04-04 03:14:06 +0800

    5.集成Ribbon

--- springcloud-consumer-dept-80/src/main/java/com/xjm/springcloud/DeptConsumer_80.java
+++ springcloud-consumer-dept-80/src/main/java/com/xjm/springcloud/DeptConsumer_80.java
@@ -2,12 +2,15 @@ package com.xjm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Panda
  * @create 2021-04-03 19:02
  */
+//Ribbon 和 Eureka 整合以后，客户端可以直接调用，不用关心IP地址和端口号
 @SpringBootApplication
+@EnableEurekaClient //开启Eureka 客户端
 public class DeptConsumer_80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_80.class,args);
