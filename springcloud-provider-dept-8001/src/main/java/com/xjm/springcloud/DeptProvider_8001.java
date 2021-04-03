1b49db1076c811a7b8f136989401436a76ec917b
JianMin-Xie <854377742@qq.com>
2021-04-03 21:07:23 +0800

    2.构建eureka-server+向eureka-server注册provider+完善监控信息

--- springcloud-provider-dept-8001/src/main/java/com/xjm/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/xjm/springcloud/DeptProvider_8001.java
@@ -2,12 +2,14 @@ package com.xjm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Panda
  * @create 2021-04-03 7:01
  */
 @SpringBootApplication
+@EnableEurekaClient//开启Eureka客户端注解，在服务启动后自动向注册中心注册服务
 public class DeptProvider_8001 {
 
     public static void main(String[] args) {
