6724844d7aabd43931d47aeb214717a6c6ce1874
JianMin-Xie <854377742@qq.com>
2021-04-03 22:37:23 +0800

    3.DiscoveryClient：注册进来的微服务，获取一些消息（团队开发会用到）

--- springcloud-provider-dept-8001/src/main/java/com/xjm/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/xjm/springcloud/DeptProvider_8001.java
@@ -2,6 +2,7 @@ package com.xjm.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient//开启Eureka客户端注解，在服务启动后自动向注册中心注册服务
+@EnableDiscoveryClient
 public class DeptProvider_8001 {
 
     public static void main(String[] args) {
