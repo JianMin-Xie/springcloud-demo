4121feac2e686619798e14da45da7a6f6b70a4ba
JianMin-Xie <854377742@qq.com>
2021-04-05 00:56:01 +0800

    7.Ribbon自定义负载均衡规则

--- springcloud-consumer-dept-80/src/main/java/com/xjm/springcloud/DeptConsumer_80.java
+++ springcloud-consumer-dept-80/src/main/java/com/xjm/springcloud/DeptConsumer_80.java
@@ -1,8 +1,10 @@
 package com.xjm.springcloud;
 
+import com.xjm.myrule.XjmRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author Panda
@@ -11,6 +13,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //Ribbon 和 Eureka 整合以后，客户端可以直接调用，不用关心IP地址和端口号
 @SpringBootApplication
 @EnableEurekaClient //开启Eureka 客户端
+//在微服务启动的时候就能加载自定义的Ribbon类(自定义的规则会覆盖原有默认的规则)
+@RibbonClient(name = "SPRINGCLOUD-PROVIDER-DEPT",configuration = XjmRule.class)//开启负载均衡,并指定自定义的规则
 public class DeptConsumer_80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_80.class,args);
