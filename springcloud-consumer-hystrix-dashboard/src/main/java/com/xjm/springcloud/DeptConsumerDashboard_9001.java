981e753ec92cfbbe86ec3ce25f3fce41032947cb
JianMin-Xie <854377742@qq.com>
2021-04-06 03:54:14 +0800

    11.Hystrix：Dashboard 流监控

---
+++ springcloud-consumer-hystrix-dashboard/src/main/java/com/xjm/springcloud/DeptConsumerDashboard_9001.java
@@ -0,0 +1,18 @@
+package com.xjm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Panda
+ * @create 2021-04-06 3:42
+ */
+@SpringBootApplication
+//开启Dashboard
+@EnableHystrixDashboard
+public class DeptConsumerDashboard_9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumerDashboard_9001.class,args);
+    }
+}
--- springcloud-provider-dept-hystrix-8001/src/main/java/com/xjm/springcloud/DeptProviderHystrix_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/xjm/springcloud/DeptProviderHystrix_8001.java
@@ -1,10 +1,13 @@
 package com.xjm.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Panda
@@ -18,5 +21,15 @@ public class DeptProviderHystrix_8001 {
 
     public static void main(String[] args) {
         SpringApplication.run(DeptProviderHystrix_8001.class,args);
+    }
+
+    //增加一个 Servlet
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        //访问该页面就是监控页面
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+
+        return registrationBean;
     }
 }
