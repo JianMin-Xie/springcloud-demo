b0763eb59ede4e18a274f2d06d6debe62d1c08c4
JianMin-Xie <854377742@qq.com>
2021-04-06 18:19:16 +0800

    12.Zuul：路由网关

---
+++ springcloud-zuul/src/main/java/com/xjm/springcloud/ZuulApplication_9527.java
@@ -0,0 +1,18 @@
+package com.xjm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author Panda
+ * @create 2021-04-06 18:10
+ */
+//Zull路由网关主启动类
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication_9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication_9527.class,args);
+    }
+}
