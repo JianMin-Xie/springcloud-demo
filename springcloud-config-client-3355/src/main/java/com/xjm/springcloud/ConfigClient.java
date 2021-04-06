10bbafd77fe07c1a867b951c9b4ed4a880839c62
JianMin-Xie <854377742@qq.com>
2021-04-07 04:24:58 +0800

    14.Spring Cloud Config：客户端

---
+++ springcloud-config-client-3355/src/main/java/com/xjm/springcloud/ConfigClient.java
@@ -0,0 +1,15 @@
+package com.xjm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Panda
+ * @create 2021-04-07 4:12
+ */
+@SpringBootApplication
+public class ConfigClient {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient.class,args);
+    }
+}
